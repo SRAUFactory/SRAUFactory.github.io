bc28c75567676d8a07f2a0123c1baa818df22237
SRAUFactory <srau.factory@gmail.com>
2018-01-07 14:53:24 +0900

    Implement app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,31 @@
-const GET_KNOWLEDGES_URL = "data/qiita.json";
+const GET_KNOWLEDGES_URL   = "data/qiita.json";
+const GET_REPOSITORIES_URL = "https://api.github.com/users/SRAUFactory/repos?per_page=100";
 
 var app = new Vue({
     el: '#knowledgesRepositores',
     data: {
-        knowledges: []
+        knowledges:   [],
+        repositories: [],
     },
     created: function () {
         this.getKnowledges();
+        this.getRepositories();
     },
     methods: {
         getKnowledges: function() {
             var self = this;
             var callback = function (json) {
                 self.knowledges = json;
-            }
+            };
             this.getData(GET_KNOWLEDGES_URL, callback);
+        },
+        getRepositories: function() {
+            var self = this;
+            var callback = function (json) {
+                self.repositories = json;
+                console.log(self.repositories);
+            };
+            this.getData(GET_REPOSITORIES_URL, callback);
         },
         getData: function(url, callback) {
             var xhr = new XMLHttpRequest();
